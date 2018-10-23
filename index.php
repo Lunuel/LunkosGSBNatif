fcd6bd5f53244243f0f990186123e1c60ceb1e80
Lunkos <kuster.lunuel@yahoo.com>
2018-10-23 18:09:04 +0200

    Débug

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-echo "p";
 require_once("include/fct.inc.php");
 require_once ("include/class.pdogsb.inc.php");
 include("vues/v_entete.php") ;
@@ -9,7 +8,7 @@ $estConnecte = estConnecte();
 if(!isset($_REQUEST['uc']) || !$estConnecte){
      $_REQUEST['uc'] = 'connexion';
 }	 
-echo 'Bonjour débutant';
+echo 'Bonjour salope';
 $uc = $_REQUEST['uc'];
 switch($uc){
 	case 'connexion':{
